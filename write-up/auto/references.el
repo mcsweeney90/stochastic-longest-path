4472339443ad35c0d6febf5144d611a182567eae
Tom <thomas.mcsweeney1990@gmail.com>
2020-10-20 18:42:32 +0100

    write up

---
+++ write-up/auto/references.el
@@ -0,0 +1,27 @@
+(TeX-add-style-hook
+ "references"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "hag88"
+    "bla08"
+    "mal59"
+    "kle71"
+    "dod85"
+    "lud01"
+    "ful62"
+    "cli64"
+    "elm67"
+    "rob76"
+    "kam85"
+    "cla61"
+    "scu83"
+    "can10"
+    "can16"
+    "vis06"
+    "zha06"
+    "van63"
+    "mar65"
+    "yaz91"
+    "kam85a"))
+ :bibtex)
+
