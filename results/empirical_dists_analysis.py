8492704125bbc0b7c8d2d2e3a6b445ee267ba9a3
Tom <thomas.mcsweeney1990@gmail.com>
2020-10-29 17:22:03 +0000

    write up

--- results/empirical_dists_analysis.py
+++ results/empirical_dists_analysis.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-
+Summaries and plots for the empirical distributions.
 """
 
 import dill, pathlib, sys
@@ -9,7 +9,7 @@ import numpy as np
 import itertools as it
 import matplotlib.pyplot as plt
 from collections import defaultdict
-sys.path.append('../') # Needed for src apparently...
+from scipy.stats import shapiro, skew, kurtosis
 
 ####################################################################################################
 
@@ -36,23 +36,49 @@ plt.ioff() # Don't show plots.
 ####################################################################################################
 
 # Destinations to save summaries and generated plots.
-summary_path = "summaries/"
+summary_path = "summaries/empirical_dists"
 pathlib.Path(summary_path).mkdir(parents=True, exist_ok=True)
-plot_path = "plots/"
+plot_path = "plots/empirical_dists"
 pathlib.Path(plot_path).mkdir(parents=True, exist_ok=True)
 
 nb = 128
 n_tasks = [35, 220, 680, 1540, 2925, 4960, 7770, 11480]
-
-# =============================================================================
-# Before runtime.
-# =============================================================================
-
 # Load info.
 with open('empirical_dists.dill', 'rb') as file:
-    empirical = dill.load(file) 
+    empirical = dill.load(file)     
+
+# =============================================================================
+# Readable summaries of tests of distribution.
+# =============================================================================
+
+with open("{}/summary_statistics.txt".format(summary_path), "w") as dest:
+    print("TESTING THE NORMALITY ASSUMPTION OF THE EMPIRICAL DISTRIBUTION FOR THE CHOLESKY DAG SET.", file=dest) 
+    print("EMPIRICAL DISTRIBUTIONS GENERATED THROUGH 100,000 REALIZATIONS OF ENTIRE GRAPH.", file=dest)
     
-
-
+    for nt in n_tasks:
+        print("\n\n\n---------------------------------", file=dest)
+        print("NUMBER OF TASKS: {}".format(nt), file=dest)
+        print("---------------------------------", file=dest)
+        for dist in ["NORMAL", "GAMMA"]: 
+            print("{} WEIGHTS".format(dist), file=dest)
+            mu = np.mean(empirical[nt][dist])
+            print("\nMEAN: {}".format(mu), file=dest)
+            var = np.var(empirical[nt][dist])
+            print("VARIANCE/STD: {} / {}".format(var, np.sqrt(var)), file=dest)
+            skw = skew(empirical[nt][dist])
+            print("SKEWNESS: {}".format(skw), file=dest)
+            kur = kurtosis(empirical[nt][dist])
+            print("EXCESS KURTOSIS: {}".format(kur), file=dest)
+            # stat, p = shapiro(empirical[nt][dist])
+            # verdict = "NORMAL" if p > 0.05 else "NOT NORMAL"
+            # print("p = {} - {}".format(p, verdict), file=dest)
+            med = np.median(empirical[nt][dist])
+            print("MEDIAN: {}".format(med), file=dest)
+            mx = max(empirical[nt][dist])
+            print("MAXIMUM: {}".format(mx), file=dest)
+            mn = min(empirical[nt][dist])
+            print("MINIMUM: {}".format(mn), file=dest)
+            
+        
 
             
